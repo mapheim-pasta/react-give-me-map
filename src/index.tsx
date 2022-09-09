6e80021b96de710acab7d1b0649750dbe1e082a3
jankondula <jan.kondula@gmail.com>
2022-09-09 10:08:48 +0200

    Youtube card

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,7 @@ export { DrawWorld } from './items/DrawWorld';
 export { ImageWorld } from './items/ImageWorld';
 export { LinkWorld } from './items/LinkWorld';
 export { PinWorld, S_DotPin } from './items/PinWorld';
+export { YoutubeWorld } from './items/YoutubeWorld';
 export { Package as GiveMeMap } from './Package';
 export { EMapStyle, ICoordinates, IViewportExtended } from './utils/map/mapTypes';
 export { isMarkerElement } from './utils/marker/markerUtils';
