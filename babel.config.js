0d5ce0ba04f1c5417ce33793f0218d82ad504072
Lukas Pastuszek <lukas.pastuszek@pipedrive.com>
2022-09-10 14:19:31 +0200

    Use rollup

---
+++ babel.config.js
@@ -0,0 +1,70 @@
+module.exports = function (babelApi) {
+    babelApi.cache(false);
+
+    return {
+        presets: [
+            [
+                '@babel/preset-env',
+                {
+                    modules: false
+                }
+            ],
+            '@babel/preset-react',
+            '@babel/preset-typescript'
+        ],
+        env: {
+            test: {
+                plugins: [
+                    '@babel/plugin-transform-modules-commonjs',
+                    'babel-plugin-dynamic-import-node-babel-7',
+                    'require-context-hook',
+                    [
+                        '@babel/plugin-transform-runtime',
+                        {
+                            absoluteRuntime: false,
+                            corejs: false,
+                            helpers: true,
+                            regenerator: true,
+                            useESModules: false
+                        }
+                    ]
+                ]
+            }
+        },
+        plugins: [
+            [
+                '@babel/plugin-proposal-decorators',
+                {
+                    legacy: true
+                }
+            ],
+            [
+                '@babel/plugin-transform-spread',
+                {
+                    loose: true
+                }
+            ],
+            [
+                '@babel/plugin-transform-runtime',
+                {
+                    absoluteRuntime: false,
+                    corejs: false,
+                    helpers: true,
+                    regenerator: false,
+                    useESModules: false
+                }
+            ],
+            '@babel/plugin-syntax-dynamic-import',
+            '@babel/plugin-proposal-class-properties',
+            [
+                'module-resolver',
+                {
+                    extensions: ['.js', '.jsx', '.ts', '.tsx'],
+                    root: ['./client'],
+                    alias: {}
+                }
+            ],
+            ['@babel/plugin-proposal-optional-chaining']
+        ]
+    };
+};
---
+++ rollup.config.js
@@ -0,0 +1,53 @@
+import cjs from '@rollup/plugin-commonjs';
+import resolve from '@rollup/plugin-node-resolve';
+import svgr from '@svgr/rollup';
+import babel from 'rollup-plugin-babel';
+import globals from 'rollup-plugin-node-globals';
+import nodePolyfills from 'rollup-plugin-node-polyfills';
+import typescript from 'rollup-plugin-typescript2';
+
+import packageJson from './package.json';
+
+export default {
+    input: './src/index',
+    output: [
+        {
+            file: packageJson.module,
+            sourcemap: true,
+            exports: 'named',
+            format: 'cjs'
+        }
+    ],
+    external: Object.keys(packageJson.peerDependencies),
+    plugins: [
+        resolve({
+            browser: true,
+            main: true,
+            preferBuiltins: false,
+            extensions: ['.ts', '.tsx', '.js', '.jsx', '.json']
+        }),
+        typescript(),
+        babel({
+            babelrc: true,
+            exclude: 'node_modules/**',
+            extensions: ['.ts', '.tsx', '.js', '.jsx'],
+            presets: ['@babel/preset-env', '@babel/preset-react'],
+            runtimeHelpers: true
+        }),
+        cjs({
+            include: 'node_modules/**'
+        }),
+        nodePolyfills(),
+        globals(),
+        svgr({
+            svgoConfig: {
+                plugins: [
+                    {
+                        name: 'removeViewBox',
+                        active: false
+                    }
+                ]
+            }
+        })
+    ]
+};
