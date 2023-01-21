5d17a2bdee395c5d523900233e03f72f82d16c49
jankondula <jan.kondula@gmail.com>
2023-01-21 17:33:07 +0100

    MapStyle

--- src/utils/map/mapTypes.ts
+++ src/utils/map/mapTypes.ts
@@ -25,7 +25,7 @@ export interface IXY {
 export enum EMapStyle {
     SATELLITE = 'mapbox://styles/koudelka/ckstetxyy07gr17o4rh01vb9b',
     OUTDOOR = 'mapbox://styles/koudelka/ckwfq7e5k45h814o8521fnwo1',
-    DEFAULT = 'mapbox://styles/koudelka/cl6gs87ey002l15o9gnp7opx7'
+    DEFAULT = 'mapbox://styles/koudelka/cld4go9oi000k01p7wci751am'
 }
 
 export interface IMapConfig {
